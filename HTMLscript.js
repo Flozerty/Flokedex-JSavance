f6e3d01832da99b26430a0650e0dce733464ffbf
Floris Louërat <floris.louerat@gmail.com>
2023-10-24 20:55:22 +0200

    tri par type

--- HTMLscript.js
+++ HTMLscript.js
@@ -1,7 +1,23 @@
-const htmlaccueil =
-    `<div class="logo">
-        <h3>Hola?</h3>
-    </div>`
-    ;
+const types = ['Acier', 'Combat', 'Dragon', 'Eau', 'Électrik', 'Fée', 'Feu', 'Glace', 'Insecte', 'Normal', 'Plante', 'Poison', 'Psy', 'Roche', 'Sol', 'Spectre', 'Ténèbres', 'Vol'];
+const generations = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-document.body.innerHTML = htmlaccueil;
+
+fetch('https://api-pokemon-fr.vercel.app/api/v1/pokemon')
+    .then(response => response.json())
+    .then(data => {
+        const pokemonByType = {};
+
+        types.forEach(type => {
+            pokemonByType[type] = getTypes(data, type);
+        });
+
+        console.log('Pokémons par type :', pokemonByType);
+    })
+    .catch(err => console.log(err));
+
+
+const getTypes = (data, type) => {
+    return data
+        .filter(pokemon => Array.isArray(pokemon.types) && pokemon.types.some(pokemonType => pokemonType.name === type))
+        .map(pokemon => pokemon.name.fr);
+}
